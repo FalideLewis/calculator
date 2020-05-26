4e92afbd1dcd5fa68ca89f6a6f495c42755499d2
Lewis <edgs2012@yandex.ru>
2020-05-26 19:52:52 +0300

    Initial commit

---
+++ 1.py
@@ -0,0 +1,20 @@
+a = int(input('input 1 number: '))
+b = int(input('input 2 number: '))
+
+z = str(input('input need act: ')) #
+
+if z == '+':
+    print(a+b)
+
+elif z == '-':
+    print(a-b)
+
+elif z == '/':
+    print(a/b)
+
+elif z == '*':
+    print(a*b)
+
+else:
+    print('select one of the possible operations: "+" "-", "/", "*" ')
+
